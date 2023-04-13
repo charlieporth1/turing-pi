f07349a04d4df0de6286fb0a624ddef8e848dfaf
Charlie Porth <charlieporth1@gmail.com>
2023-04-13 13:56:54 -0500

    ci fix: Thu Apr 13 13:56:51 CDT 2023

--- ctp-scripts/build.sh
+++ ctp-scripts/build.sh
@@ -12,6 +12,13 @@ curl https://pkgs.tailscale.com/stable/${NAME}.tgz --output ${NAME}.tgz
 tar -xvf ${NAME}.tgz
 cp -rf ${NAME}/tailscale br2t113pro/board/100ask/rootfs_overlay/bin/tailscale
 cp -rf ${NAME}/tailscaled br2t113pro/board/100ask/rootfs_overlay/bin/tailscaled
+
+cp -rf ${NAME}/tailscale buildroot/system/skeleton/usr/bin/
+cp -rf ${NAME}/tailscaled buildroot/system/skeleton/usr/bin/
+
 chmod +x br2t113pro/board/100ask/rootfs_overlay/bin/tailscale
 chmod +x br2t113pro/board/100ask/rootfs_overlay/bin/tailscaled
+
+chmod +x buildroot/system/skeleton/usr/bin/
+chmod +x buildroot/system/skeleton/usr/bin/
 
