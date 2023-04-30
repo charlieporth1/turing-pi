c3d9886c4cd04693e2b27627d9e6dd75615e83f7
Charlie  Porth <charlieporth@Charlies-MacBook-Pro.local>
2023-04-29 19:52:19 -0500

    fixed ci: Sat Apr 29 19:52:19 CDT 2023

--- ctp-scripts/build.sh
+++ ctp-scripts/build.sh
@@ -11,21 +11,21 @@ curl https://pkgs.tailscale.com/stable/${NAME}.tgz --output ${NAME}.tgz
 
 #mkdir -p br2t113pro/board/100ask/rootfs_overlay/mnt/bin
 tar -xvf ${NAME}.tgz
-#cp -rf ${NAME}/tailscale br2t113pro/board/100ask/rootfs_overlay/bin/
-#cp -rf ${NAME}/tailscaled br2t113pro/board/100ask/rootfs_overlay/bin/
+cp -rf ${NAME}/tailscale br2t113pro/board/100ask/rootfs_overlay/bin/
+cp -rf ${NAME}/tailscaled br2t113pro/board/100ask/rootfs_overlay/bin/
 
 #cp -rf ${NAME}/tailscale br2t113pro/board/100ask/rootfs_overlay/mnt/bin
 #cp -rf ${NAME}/tailscaled br2t113pro/board/100ask/rootfs_overlay/mnt/bin
 
-cp -rf ${NAME}/tailscale buildroot/system/skeleton/usr/bin/
-cp -rf ${NAME}/tailscaled buildroot/system/skeleton/usr/bin/
+#cp -rf ${NAME}/tailscale buildroot/system/skeleton/usr/bin/
+#cp -rf ${NAME}/tailscaled buildroot/system/skeleton/usr/bin/
 
-#chmod +x br2t113pro/board/100ask/rootfs_overlay/bin/tailscaled
-#chmod +x br2t113pro/board/100ask/rootfs_overlay/bin/tailscale
+chmod +x br2t113pro/board/100ask/rootfs_overlay/bin/tailscaled
+chmod +x br2t113pro/board/100ask/rootfs_overlay/bin/tailscale
 
 #chmod +x br2t113pro/board/100ask/rootfs_overlay/mnt/bin/tailscale
 #chmod +x br2t113pro/board/100ask/rootfs_overlay/mnt/bin/tailscaled
 
-chmod +x buildroot/system/skeleton/usr/bin/tailscale
-chmod +x buildroot/system/skeleton/usr/bin/tailscaled
+#chmod +x buildroot/system/skeleton/usr/bin/tailscale
+#chmod +x buildroot/system/skeleton/usr/bin/tailscaled
 
